c7fd24dff244284c7fac9869bab071c22f774262
DIMFLIX <dimflix.official@gmail.com>
2025-12-21 15:29:17 +0300

    Added additional theme application commands for bspwm

--- configs/bspwm/pawlette-catppuccin-latte.sh
+++ configs/bspwm/pawlette-catppuccin-latte.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Using .Xresources for X11 applications
+[ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
+
 # Set the border colors for focused and normal windows
 bspc config focused_border_color "#7287fd"
 bspc config border_width 3
@@ -21,3 +24,6 @@ gsettings set org.gnome.desktop.interface icon-theme 'pawlette-catppuccin-latte'
 # Set cursor theme
 gsettings set org.gnome.desktop.interface cursor-theme 'pawlette-catppuccin-latte'
 gsettings set org.gnome.desktop.interface cursor-size 20
+
+# Use the cursor for X11 (if necessary)
+xsetroot -cursor_name left_ptr
